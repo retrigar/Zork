6da4a925bc9c8784669f76782a8f55c9674b35b4
retrigar <42656578+retrigar@users.noreply.github.com>
2020-09-10 18:40:32 -0400

    Zork 1.2 complete

---
+++ Zork/Commands.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zork
+{
+    enum Commands
+    {
+        QUIT,
+        LOOK,
+        NORTH,
+        SOUTH,
+        EAST,
+        WEST,
+        UNKNOWN
+    }
+}
