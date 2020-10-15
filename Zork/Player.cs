a57e375e777fdb28932f0dad67bc333ad5a7c0c6
retrigar <42656578+retrigar@users.noreply.github.com>
2020-10-15 18:42:47 -0400

    4.3 complete

---
+++ Zork/Player.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zork
+{
+   /* public enum Directions
+    {
+        North,
+        South,
+        East,
+        West
+    }*/
+    public class Player
+    {
+        public World World { get; }
+
+        [JsonIgnore]
+        public Room Location { get; private set; }
+
+        [JsonIgnore]
+        public string LocationName
+        {
+            get
+            {
+                return Location?.Name;
+            }
+            set
+            {
+                Location = World?.RoomsByName.GetValueOrDefault(value);
+            }
+        }
+
+        public Player(World world, string startingLocation)
+        {
+            World = world;
+            LocationName = startingLocation;
+        }
+
+        public bool Move(Directions direction)
+        {
+            bool isValidMove = Location.Neighbors.TryGetValue(direction, out Room destination);
+            if (isValidMove)
+            {
+                Location = destination;
+            }
+
+            return isValidMove;
+        }
+
+    }
+}
